d7ce3960a003587bd445fc28f062860483971588
Jayant Sai <jayant.b.sai@gmail.com>
2024-09-11 22:50:36 -0700

    Update test_demo.py
    Update tests to use regex instead of 'word' in *

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,31 +1,33 @@
 import pytest
+import re
 
 from demo import Demo
 from llmtoolutil import llm_tool_util
 
-@pytest.mark.parametrize('prompt, expected_list_of_words', [
+@pytest.mark.parametrize('prompt, regexs', [
     (
         'Who was the first president of the united states?',
-        ['George Washington']
+        [r'George Washington']
     ),
     (
         'What will the temperature be in London, next Monday?',
-        ['temperature', 'precipitation', 'wind speed', 'London', 'Monday']
+        [r'\btemperature\b|\bweather\b', r'precipitation', r'wind speed', r'London', r'Monday']
     ),
     (
         'Who were the top 3 gold medal winning countries in the Tokyo olympics?',
-        ['United States', 'China', 'Japan', 'Tokyo Olympics']
+        [r'United States', r'China', r'Japan', r'Tokyo Olympics']
     ),
     (
         'What will be the weather in San Francisco on Friday?',
-        ['weather', 'San Francisco', 'Friday']
+        [r'\btemperature\b|\bweather\b', r'San Francisco', r'Friday']
     )
 ])
 
-def test_demo_request(prompt:str, expected_list_of_words:list):
+def test_demo_request(prompt:str, regexs:list):
     assert('get_weather_forecast' in llm_tool_util._tool_funcs)
 
     assistant = Demo()
     response = assistant.handle(prompt)
-    for word in expected_list_of_words:
-        assert(word.lower() in response.lower())
+    
+    for regx in regexs:
+        assert(re.search(regx, response.lower(), re.IGNORECASE) != None)
